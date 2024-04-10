61e12f2462ff49f697e6833c1cdeb844a2c3b877
roel.edeleon <roel.edeleon@gmail.com>
2024-04-10 23:16:59 +0800

    20240410 2316H

--- src_/index.js
+++
@@ -1,6 +0,0 @@
-import './js/mylibrary_update-details';
-import './js/modal_project-team';
-import './js/modal_search-film-card';
-import './js/modal_fetch-film-card';
-import './js/search-movies';
-import './js/fetch-movies';
--- src_/mylibrary.js
+++
@@ -1,4 +0,0 @@
-import './js/modal_project-team';
-import './js/modal_mylibrary-queue-film-card';
-import './js/modal_mylibrary-watch-film-card';
-import './js/mylibrary_fetch-movies';
